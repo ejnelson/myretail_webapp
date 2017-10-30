077ddbdd976b1b34fd4d207fa00de381b3fe70bd
Nelson, Erik-CW <Erik.Nelson2@bestbuy.com>
2017-10-30 18:38:46 -0500

    folder structure and component architecture

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
+import ItemPreview from '../ItemPreview';
+import ItemDetails from '../ItemDetails';
+import ItemRatings from '../ItemRatings';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <ItemPreview />
+    <ItemDetails />
+    <ItemRatings />
+  </div>
+);
 
 export default App;
