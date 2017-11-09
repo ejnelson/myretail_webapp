36b20b3b141db40baa7ecb8d28ae2c49ce8e3e8f
Nelson, Erik-CW <Erik.Nelson2@bestbuy.com>
2017-11-08 19:49:50 -0600

    finish up ratings css

--- src/components/ItemRatings/ProConRatings.js
+++ src/components/ItemRatings/ProConRatings.js
@@ -9,6 +9,25 @@ export default class ProConRatings extends Component {
 
   render() {
     const { proRatingObject, conRatingObject } = this.props;
+    const proStarArray = [];
+    const conStarArray = [];
+    for (let i = 0; i < 5; i += 1) {
+      if (i < proRatingObject.overallRating) {
+        proStarArray.push('red');
+      } else {
+        proStarArray.push('grey');
+      }
+    }
+    for (let i = 0; i < 5; i += 1) {
+      if (i < conRatingObject.overallRating) {
+        conStarArray.push('red');
+      } else {
+        conStarArray.push('grey');
+      }
+    }
+    const proDatePosted = new Date(proRatingObject.datePosted);
+    const conDatePosted = new Date(conRatingObject.datePosted);
+
     return (
       <div className="ProConRatings">
         <div className="proConHeader">
@@ -21,8 +40,42 @@ export default class ProConRatings extends Component {
             <h6>most helpful 1-2 star review</h6>
           </div>
         </div>
-        <p>{proRatingObject.review}</p>
-        <p>{conRatingObject.review}</p>
+        <div className="proConBody">
+          <div className="proBody">
+            {proStarArray.map((color, index) => (
+              <i className={`el el-star ${color}`} key={index} />
+            ))}
+            <h5>{proRatingObject.title}</h5>
+            <p>{proRatingObject.review}</p>
+            <span>
+              <a href="#">{proRatingObject.screenName}</a>
+            </span>
+            <span>
+              {`${proDatePosted.toLocaleString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+              })}`}
+            </span>
+          </div>
+          <div className="proBody">
+            {conStarArray.map((color, index) => (
+              <i className={`el el-star ${color}`} key={index} />
+            ))}
+            <h5>{conRatingObject.title}</h5>
+            <p>{conRatingObject.review}</p>
+            <span>
+              <a href="#">{conRatingObject.screenName}</a>
+            </span>
+            <span>
+              {`${conDatePosted.toLocaleString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+              })}`}
+            </span>
+          </div>
+        </div>
       </div>
     );
   }
